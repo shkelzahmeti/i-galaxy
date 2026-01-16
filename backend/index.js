3f09e95e057587475e624ca58e72d614d8ec8b14
Shkelz Ahmeti <imshkelz@gmail.com>
2026-01-16 15:11:07 +0100

    added backend

---
+++ backend/index.js
@@ -0,0 +1,135 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+//Database connection with MongoDB:
+
+mongoose.connect(
+  "mongodb+srv://imshkelz_db_user:8Rsv3Sej3I5hnOzI@cluster0.rbjwb9d.mongodb.net/i-galaxy"
+);
+
+//API Creation
+app.listen(port, (error) => {
+  if (!error) {
+    console.log("Server Running on Port " + port);
+  } else {
+    console.log("Error: " + error);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Express App is Running");
+});
+
+// Image Storage Engine:
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// Creating Upload Endpoint for Images:
+app.use("/images", express.static("upload/images"));
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    imageUrl: `http://localhost:${port}/images/${req.file.fileName}`,
+  });
+});
+
+// Schema for Creating Products:
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: { type: String, required: true },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  newPrice: {
+    type: Number,
+    required: true,
+  },
+  oldPrice: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+// Adding Product to Database:
+app.post("/addproduct", async (req, res) => {
+  let products = await Product.find({});
+  let id;
+  if (products.length > 0) {
+    let lastProductArray = products.slice(-1);
+    let lastProduct = lastProductArray[0];
+    id = lastProduct.id + 1;
+  } else {
+    id = 1;
+  }
+  const product = new Product({
+    // id: req.body.id,
+    id: id,
+    name: req.body.name,
+    image: req.body.image,
+    category: req.body.category,
+    newPrice: req.body.newPrice,
+    oldPrice: request.body.oldPrice,
+  });
+  console.log(product);
+  await product.save();
+  console.log("Saved");
+  response.json({
+    success: true,
+    name: request.body.name,
+  });
+});
+
+// Creating API for Deleting Products:
+app.post("/removeproduct", async (req, res) => {
+  await Product.findOneAndDelete({ id: req.body.id });
+  console.log("Removed");
+  res.json({ success: true, name: req.body.name });
+});
+
+// Creating API for Getting All Products:
+app.get("/allproducts", async (req, res) => {
+  let products = await Product.find({});
+  console.log("All Products Fetched");
+  res.send(products);
+});
+
+// RUN THE SERVER:
+// Type `node .\index.js` in Terminal to Run the Server,
+// then on Browser: `localhost:4000`
+
+// USING THESE ENDPOINTS WE'LL CREATE THE ADMIN PANEL
+// FOR OUR WEBSITE.
