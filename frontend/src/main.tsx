b98047d22ea9d65fd4782ad279ce92e7f123ed74
Shkelz Ahmeti <imshkelz@gmail.com>
2026-01-17 11:08:32 +0100

    added signup function

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import ShopContextProvider from "./context/ShopContextProvider.tsx";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* Providing data to all pages */}
     <ShopContextProvider>
       <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        toastStyle={{ fontSize: "1.6rem" }}
+      />
     </ShopContextProvider>
-  </StrictMode>
+  </StrictMode>,
 );
--- frontend/src/pages/LoginSignup.tsx
+++ frontend/src/pages/LoginSignup.tsx
@@ -1,25 +1,120 @@
-import ".//css/LoginSignup.css";
+import { useState } from "react";
+import "./css/LoginSignup.css";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+interface FormData {
+  userName: string;
+  password: string;
+  email: string;
+}
+
+const initialForm: FormData = {
+  userName: "",
+  password: "",
+  email: "",
+};
+interface SignupResponse {
+  success: boolean;
+  token: string;
+  errors: string;
+}
 
 function LoginSignup() {
+  const [state, setState] = useState<string>("Login");
+  const [formData, setFormData] = useState<FormData>(initialForm);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSignup = async () => {
+    console.log("Login function executed", formData);
+
+    let responseData!: SignupResponse;
+    await fetch("http://localhost:4000/signup", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        responseData = data;
+      });
+
+    if (responseData.success) {
+      localStorage.setItem("auth-token", responseData.token);
+      toast.success("You've signed up successfully");
+      navigate("/");
+    } else {
+      toast.error(responseData.errors);
+    }
+  };
+
+  const handleLogin = async () => {
+    console.log("Signup function executed", formData);
+  };
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (state === "Login") handleLogin();
+    else handleSignup();
+  };
+
   return (
-    <section className="section-loginsignup">
+    <form onSubmit={handleFormSubmit} className="form-loginsignup">
       <div className="loginsignup-container">
-        <h2>Sign Up</h2>
+        <h2>{state}</h2>
         <div className="loginsignup-fields">
-          <input type="text" placeholder="Your Name" />
-          <input type="email" placeholder="Email Address" />
-          <input type="password" placeholder="Password" />
+          {state === "Sign Up" ? (
+            <input
+              onChange={handleChange}
+              name="userName"
+              value={formData.userName}
+              type="text"
+              placeholder="Your Name"
+            />
+          ) : (
+            <></>
+          )}
+          <input
+            onChange={handleChange}
+            name="email"
+            value={formData.email}
+            type="email"
+            placeholder="Email Address"
+          />
+          <input
+            onChange={handleChange}
+            name="password"
+            value={formData.password}
+            type="password"
+            placeholder="Password"
+          />
         </div>
-        <button>Continue</button>
-        <p className="loginsignup-login">
-          Already have an account? <span>Login here</span>
-        </p>
+        <button type="submit">Continue</button>
+        {state === "Sign Up" ? (
+          <p className="loginsignup-login">
+            Already have an account?{" "}
+            <span onClick={() => setState("Login")}>Login here</span>
+          </p>
+        ) : (
+          <p className="loginsignup-login">
+            Create an account{" "}
+            <span onClick={() => setState("Sign Up")}>Signup here</span>
+          </p>
+        )}
         <div className="loginsignup-agree">
           <input type="checkbox" name="" id="" />
           <p>By continuing, I agree to the terms of use & privacy policy</p>
         </div>
       </div>
-    </section>
+    </form>
   );
 }
 
